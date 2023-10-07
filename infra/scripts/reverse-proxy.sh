2faa6c007e24be61046e3159e43f664ece54fdfd
Saptaktdk <dassaptak0@gmail.com>
2023-10-08 02:58:26 +0530

    Added scripts for ssh ing in vm

---
+++ infra/scripts/reverse-proxy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#? Install nginx into the VM
+sudo apt install nginx
+
+#? Check status of nginx
+sudo systemctl status nginx
+
+#? Configure nginx
+server { }
---
+++ infra/scripts/ssh-vm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+VM_EXTERNAL_IP="35.238.14.183"
+RUNNER_PRIVATE_KEY_PATH=/home/saptak/.ssh/runner
+GITHUB_PRIVATE_KEY_PATH=/home/saptak/.ssh/github
+USER_NAME=runner
+
+#? SSH into the VM
+eval `ssh-agent`
+
+scp -i $RUNNER_PRIVATE_KEY_PATH $GITHUB_PRIVATE_KEY_PATH $USER_NAME@$VM_EXTERNAL_IP:/home/$USER_NAME/.ssh/id_ed25519
+
+ssh-add $PRIVATE_KEY_PATH
+
+ssh -i $RUNNER_PRIVATE_KEY_PATH $USER_NAME@$VM_EXTERNAL_IP 
+
